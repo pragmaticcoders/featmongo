abfd4bdec92b51617f73695fdfc1a96f9267759f
Mariusz Bidzinski <mariusz.bidzinski@pragmaticcoders.com>
2014-11-24 22:12:36 +0100

    Additional parameters in find_with_applied_manipulators helper

--- featmongo/helpers.py
+++ featmongo/helpers.py
@@ -1,5 +1,5 @@
-def find_with_applied_manipulators(collection, query):
+def find_with_applied_manipulators(collection, query, **kwargs):
     db = collection.database
     manip_query = db._apply_incoming_manipulators(query, collection)
-    coursor = collection.find(manip_query, manipulate=True)
+    coursor = collection.find(manip_query, manipulate=True, **kwargs)
     return coursor
