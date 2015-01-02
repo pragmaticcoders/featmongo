416d852af74c11d7d1d8b28b638e65b82212ad79
Mariusz Bidzinski <mariusz.bidzinski@pragmaticcoders.com>
2015-01-02 10:39:52 +0100

    New helper - update_with_applied_manipulators

--- featmongo/helpers.py
+++ featmongo/helpers.py
@@ -4,8 +4,17 @@ def find_with_applied_manipulators(collection, query, **kwargs):
     coursor = collection.find(manip_query, manipulate=True, **kwargs)
     return coursor
 
+
 def find_one_with_applied_manipulators(collection, query, **kwargs):
     db = collection.database
     manip_query = db._apply_incoming_manipulators(query, collection)
     feat_object = collection.find_one(manip_query, manipulate=True, **kwargs)
     return feat_object
+
+
+def update_with_applied_manipulators(collection, query, feat_obj, **kwargs):
+    db = collection.database
+    manip_query = db._apply_incoming_manipulators(query, collection)
+    manip_object = db._apply_incoming_manipulators(feat_obj, collection)
+    update_value = collection.update(manip_query, manip_object, manipulate=True, **kwargs)
+    return update_value
